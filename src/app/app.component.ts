22c43d6e1b9dc3f42c45b4245bc7e121e69b9a04
gllryns <gllryns@gmail.com>
2022-10-20 21:10:19 -0300

    Correccion de bugs en promesa y observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, mergeMap, of, map, filter } from 'rxjs';
+import { ReactiveService } from './reactive.service';
+import { Observable, Subscription } from 'rxjs';
+import { Curso } from './curso';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'ProgramacionReactivaReynosoG';
+  numbers$ = interval(1000);
+  numbersSuscription!: Subscription;
+  number!: Number;
+
+  cursos!: Curso[];
+  cursos$: Observable<Curso[]>;
+  suscripcion: any;
+  promesa: any;
+
+  filterMe$: Observable<number> = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).pipe(
+    filter((v) => v % 2 === 0)
+  );
+  filterMeSubscription!: Subscription;
+
+  constructor(private reactiveService: ReactiveService) {
+    this.promesa = reactiveService.obtenerCursosPromise();
+
+    this.suscripcion = reactiveService.obtenerCursosObservable().subscribe({
+      next: (cursos: Curso[]) => {
+        this.cursos = cursos;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+    this.cursos$ = reactiveService.obtenerCursosObservable();
+  }
+  ngOnInit(): void {
+    this.numbersSuscription = this.reactiveService
+      .getInterval()
+      .subscribe((number) => (this.number = number + 5));
+
+    this.filterMeSubscription = this.filterMe$.subscribe(
+      (next) => (this.number = next),
+      (error) => console.error(error)
+    );
+  }
+  ngOnDestroy(): void {
+    this.numbersSuscription.unsubscribe();
+    this.suscripcion.unsubscribe();
+  }
+  agregarCurso() {
+    let curso: Curso = {
+      nombre: 'TypeScript',
+      profesor: 'Yolanka',
+    };
+    this.reactiveService.agregarCurso(curso);
+  }
 }
--- src/app/reactive.service.ts
+++ src/app/reactive.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, interval, Observable } from 'rxjs';
+import { Curso } from './curso';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReactiveService {
+  numbers$ = interval(1000);
+  cursos$: Observable<Curso[]>;
+  cursosSubject: BehaviorSubject<Curso[]>;
+  cursos: Curso[] = [
+    {
+      nombre: 'Angular',
+      profesor: 'Martin Karadagian',
+    },
+    {
+      nombre: 'ReactJS',
+      profesor: 'La momia',
+    },
+    {
+      nombre: 'Kotlin',
+      profesor: 'Caballero Rojo',
+    },
+  ];
 
-  constructor() { }
+  public getInterval() {
+    return this.numbers$;
+  }
+
+  obtenerCursosPromise(): Promise<Curso[] | any> {
+    return new Promise((resolve, reject) => {
+      if (this.cursos.length > 0) {
+        resolve(this.cursos);
+      } else {
+        reject({
+          codigo: 0,
+          mensaje: 'No hay cursos disponibles en este momento',
+        });
+      }
+    });
+  }
+
+  obtenerCursosObservable() {
+    return this.cursosSubject.asObservable();
+  }
+  agregarCurso(curso: Curso) {
+    this.cursos.push(curso);
+    this.cursosSubject.next(this.cursos);
+  }
+
+  constructor() {
+    this.cursosSubject = new BehaviorSubject<Curso[]>(this.cursos);
+
+    this.cursos$ = new Observable<Curso[]>((suscriptor) => {
+      suscriptor.next(this.cursos);
+
+      setTimeout(() => {
+        this.cursos.push({
+          nombre: 'VueJS',
+          profesor: 'Ruben Peucelle',
+        });
+        suscriptor.next(this.cursos);
+      }, 2000);
+    });
+  }
 }
